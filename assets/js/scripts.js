4c3451a248ed1163492094e7088d2fac2603a50b
Eric Slutz <eric.slutz@icloud.com>
2021-09-06 23:36:17 -0400

    Almost everything working with mobile menu now.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,13 @@
 window.onload = function copyrightWithCurrentYear() {
   const year = new Date().getFullYear();
   document.getElementById("copyright").innerHTML = `&copy; ${year}.&nbsp;`;
-  console.log(`current year: ${year}`);
 };
+
+function showMenu() {
+  var x = document.getElementById("nav_links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
