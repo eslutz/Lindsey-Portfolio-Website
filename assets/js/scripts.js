0c145bd16e390deb31b832c64a966ea4cd6c1e60
Eric Slutz <eric.slutz@icloud.com>
2021-09-09 22:31:08 -0400

    Fixed some styling and added scripting for work examples.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,3 +11,36 @@ function showMenu() {
     x.style.display = "block";
   }
 }
+
+function makeActive(newActiveSection) {
+  var currentSection = document.getElementById(newActiveSection);
+  currentSection.classList.add("work-page-active");
+
+  var vaDoeSection = document.getElementById("va_doe_section");
+  var lettuceHelpSection = document.getElementById("lettuce_help_section");
+  var movinstSection = document.getElementById("movist_section");
+  var gradCertection = document.getElementById("grad_cert_section");
+
+  switch (newActiveSection) {
+    case "va_doe_section":
+      lettuceHelpSection.classList.remove("work-page-active");
+      movinstSection.classList.remove("work-page-active");
+      gradCertection.classList.remove("work-page-active");
+      break;
+    case "lettuce_help_section":
+      vaDoeSection.classList.remove("work-page-active");
+      movinstSection.classList.remove("work-page-active");
+      gradCertection.classList.remove("work-page-active");
+      break;
+    case "movist_section":
+      lettuceHelpSection.classList.remove("work-page-active");
+      vaDoeSection.classList.remove("work-page-active");
+      gradCertection.classList.remove("work-page-active");
+      break;
+    case "grad_cert_section":
+      lettuceHelpSection.classList.remove("work-page-active");
+      vaDoeSection.classList.remove("work-page-active");
+      movinstSection.classList.remove("work-page-active");
+      break;
+  }
+}
